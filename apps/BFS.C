33a3651ff1b6621780a74cb37c2885b4598c29ff
anuchak <anurag.chakraborty98@gmail.com>
2025-04-20 14:22:50 -0400

    add changes

--- apps/BFS.C
+++ apps/BFS.C
@@ -23,8 +23,25 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <chrono>
 #include <omp.h>
+#include <thread>
+#include <vector>
 #include "../ligra/ligra.h"
 #include <algorithm>
+#include <atomic>
+#include <condition_variable>
+
+template<class vertex>
+struct TaskInfo {
+    std::mutex lock;
+    std::condition_variable cond;
+
+    alignas(64) atomic<bool> finalExit{false};
+    alignas(64) atomic<long> sourceStart{0llu};
+    alignas(64) atomic<long> sourceEnd{0llu};
+    graph<vertex> &GA;
+
+    explicit TaskInfo(graph<vertex> &GA) : GA(GA) {}
+};
 
 struct BFS_F {
     uintE *Parents;
@@ -50,59 +67,93 @@ struct BFS_F {
 };
 
 template<class vertex>
+void workerThreadFunc(TaskInfo<vertex> &info) {
+    auto &mutex = info.lock;
+    auto &cv = info.cond;
+    std::unique_lock<std::mutex> lck(mutex, std::defer_lock);
+    while (true) {
+        lck.lock();
+        cv.wait(lck, [&] {
+            if (info.finalExit.load(std::memory_order_acquire)) {
+                return true;
+            }
+            if (info.sourceStart.load(std::memory_order_acquire)
+                != info.sourceEnd.load(std::memory_order_acquire)) {
+                return true;
+            }
+            return false;
+        });
+        lck.unlock();
+        if (info.finalExit.load(std::memory_order_acquire)) {
+            return;
+        }
+        auto currSource = 0u;
+        while ((currSource = info.sourceStart.fetch_add(1u)) < info.sourceEnd.load(memory_order_acquire)) {
+            long n = info.GA.n;
+            //creates Parents array, initialized to all -1, except for start
+            uintE *Parents = newA(uintE, n);
+            parallel_for (long i = 0; i < n; i++) Parents[i] = UINT_E_MAX;
+            Parents[currSource] = currSource;
+            vertexSubset Frontier(n, currSource); //creates initial frontier
+            int level = 0;
+            while (!Frontier.isEmpty()) {
+                vertexSubset output = edgeMap(info.GA, Frontier, BFS_F(Parents));
+                Frontier.del();
+                Frontier = output; //set new frontier
+                level++;
+                printf("source: %u | level: %d | size: %lu\n", currSource, level, Frontier.size());
+            }
+            Frontier.del();
+            free(Parents);
+        }
+        cv.notify_all();
+    }
+}
+
+template<class vertex>
 void Compute(graph<vertex> &GA, commandLine P) {
-    auto totalThreads = omp_get_max_threads();
-    // hardcoding 'k' value, launch 2 sources
-    auto k = 2;
-    // enable nested parallelism
-    omp_set_nested(1);
+    // hardcoding 'k' value, base case is 1 source
+    auto k = 1;
+    auto workerThreads = std::vector<std::thread>();
+    TaskInfo<vertex> taskInfo {GA};
+    for (auto i = 0u; i < k; i++) {
+        workerThreads.emplace_back([&] {
+            workerThreadFunc(taskInfo);
+        });
+    }
+    std::unique_lock<std::mutex> lck(taskInfo.lock, std::defer_lock);
     for (auto round = 0u; round < 2u; round++) {
         auto duration = std::chrono::system_clock::now().time_since_epoch();
         auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-        // re-using the rounds parameter for total sources to perform BFS for
         unsigned sourceStart = P.getOptionLongValue("-sourceStart", 0);
         unsigned sourceEnd = P.getOptionLongValue("-sourceEnd", 0);
-        for (auto source = sourceStart; source < sourceEnd; source += k) {
-            // printf("source: %lu \n", source);
-            // auto duration2 = std::chrono::system_clock::now().time_since_epoch();
-            // auto millis2 = std::chrono::duration_cast<std::chrono::milliseconds>(duration2).count();
-
-            auto batchEnd = std::min(source + k, sourceEnd);
-            auto totalThreadsToLaunch = batchEnd - source;
-            int threadsPerSource = std::ceil(totalThreads / totalThreadsToLaunch);
-            printf("max threads available: %d | threads to launch per source: %d | total sources launched: %d\n",
-                totalThreads, threadsPerSource, totalThreadsToLaunch);
-
-            #pragma omp parallel for num_threads(totalThreadsToLaunch) schedule(static)
-            for (auto batchStart = source; batchStart < batchEnd; batchStart++) {
-                // add BFS Logic
-                int tid = omp_get_thread_num();
-                printf("Thread: %d | Source: %u\n", tid, batchStart);
-                long n = GA.n;
-                //creates Parents array, initialized to all -1, except for start
-                uintE *Parents = newA(uintE, n);
-                parallel_for(long i = 0; i < n; i++) Parents[i] = UINT_E_MAX;
-                Parents[source] = source;
-                vertexSubset Frontier(n, source); //creates initial frontier
-                int level = 0;
-                while (!Frontier.isEmpty()) {
-                    vertexSubset output = edgeMapLimitedThreads(GA, Frontier, BFS_F(Parents), threadsPerSource);
-                    Frontier.del();
-                    Frontier = output; //set new frontier
-                    level++;
-                    printf("level: %d | %lu\n", level, Frontier.size());
-                    // if some source is not connected at all (no work), then increment total sources given
-                    // ensure fairness of work, exactly 1 / 8 / 64 sources with work are used for BFS
-                    if (level == 1 && Frontier.size() == 0) {
-                        sourceEnd++;
-                    }
-                }
-                Frontier.del();
-                free(Parents);
+        auto source = sourceStart;
+        while (true) {
+            auto end = std::min(source + k, sourceEnd);
+            taskInfo.lock.lock();
+            taskInfo.sourceStart.store(source, memory_order_release);
+            taskInfo.sourceEnd.store(end, memory_order_release);
+            taskInfo.lock.unlock();
+            taskInfo.cond.notify_all();
+            lck.lock();
+            taskInfo.cond.wait(lck, [&] {
+               return taskInfo.sourceStart.load(std::memory_order_acquire) >= end;
+            });
+            lck.unlock();
+            if (end == sourceEnd) {
+                break;
             }
+            source = end;
         }
         auto duration1 = std::chrono::system_clock::now().time_since_epoch();
         auto millis1 = std::chrono::duration_cast<std::chrono::milliseconds>(duration1).count();
         printf("total time: %lu\n", millis1 - millis);
+    }
+    taskInfo.lock.lock();
+    taskInfo.finalExit.store(true, memory_order_release);
+    taskInfo.lock.unlock();
+    taskInfo.cond.notify_all();
+    for (auto &thread : workerThreads) {
+        thread.join();
     }
 }
